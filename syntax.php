7cde9350097e6527ea4f5abe44383a9fc7bf4583
Niklas Fejes <niklas.fejes@colorado.edu>
2017-01-18 12:44:23 +0100

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -21,7 +21,7 @@ require_once(DOKU_PLUGIN.'syntax.php');
  * All DokuWiki plugins to extend the parser/rendering mechanism
  * need to inherit from this class
  */
-class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
+class syntax_plugin_googlecal2 extends DokuWiki_Syntax_Plugin {
 
     function getType() { return 'substition'; }
     
@@ -36,7 +36,7 @@ class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
     function handle($match, $state, $pos, Doku_Handler $handler){        
         if(preg_match('/{{cal2>(.*)/', $match)) {             // Hook for future features
             // Handle the simplified style of calendar tag
-            $match = html_entity_decode(substr($match, 6, -2));
+            $match = html_entity_decode(substr($match, 7, -2));
             @list($url, $alt) = explode('|',$match,2);
             $matches = array();
             
