79cf37f4a455362139e08a3ac2a065655ac8e944
Niklas Fejes <niklas.fejes@colorado.edu>
2017-01-18 12:27:27 +0100

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Plugin googlecal2: Inserts an Google Calendar iframe
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Niklas Fejes (@nfejes)
+ *
+ *
  * Plugin googlecal: Inserts an Google Calendar iframe
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
@@ -24,11 +30,11 @@ class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
     function getSort() { return 319; }
     
     function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('{{cal>[^}]*?}}', $mode, 'plugin_googlecal');
+        $this->Lexer->addSpecialPattern('{{cal2>[^}]*?}}', $mode, 'plugin_googlecal2');
     }
 
     function handle($match, $state, $pos, Doku_Handler $handler){        
-        if(preg_match('/{{cal>(.*)/', $match)) {             // Hook for future features
+        if(preg_match('/{{cal2>(.*)/', $match)) {             // Hook for future features
             // Handle the simplified style of calendar tag
             $match = html_entity_decode(substr($match, 6, -2));
             @list($url, $alt) = explode('|',$match,2);
@@ -41,32 +47,39 @@ class syntax_plugin_googlecal extends DokuWiki_Syntax_Plugin {
                     @list($w, $h) = explode(',',$matches[2],2);
                 } else {
                     $h = $matches[2];
-                    $w = '98%';
+                    $w = '100%';
                 }
             } else {
-                $w = '98%';
+                $w = '100%';
                 $h = '600';
             }
             if (!isset($alt)) $alt = '';
 
+            $css = "border: solid 2px black;box-sizing: border-box;";
+            if (substr($w,-1) === "%") {
+                $css .= "width: $w;";
+                $w = '800';
+            }
+            
             if (!$this->getConf('js_ok') && substr($url,0,11) == 'javascript:') {
                 return array('error', $this->getLang('gcal_No_JS'));
             }
-            return array('wiki', hsc(trim("$url")), hsc(trim($alt)), hsc(trim($w)), hsc(trim($h)));
+            return array('wiki', hsc(trim("$url")), hsc(trim($alt)), hsc(trim($w)), hsc(trim($h)), hsc(trim($css)));
         } else {
             return array('error', $this->getLang("gcal_Bad_iFrame"));  // this is an error
         } // matched {{cal>...
     }
 
     function render($mode, Doku_Renderer $renderer, $data) {
-        list($style, $url, $alt, $w, $h) = $data;
+        list($style, $url, $alt, $w, $h, $css) = $data;
         
         if($mode == 'xhtml'){
             // Two styles: wiki and error
             switch($style) {
                 case 'wiki':
-                    $renderer->doc .= "<iframe src='https://www.google.com/calendar/embed?src=$url&amp;height=$h&amp;title=$alt'".
-                "title='$alt'  width='$w' height='$h' frameborder='0'></iframe>\n";
+                    $src = "https://calendar.google.com/calendar/embed?src=$url";
+                    $renderer->doc .= "<iframe src='$src' title='$alt' width='$w' height='$h' " . 
+                                      "frameborder='0' scrolling='no' style='$css'></iframe>\n";
                     break;
                 case 'error':
                     $renderer->doc .= "<div class='error'>$url</div>";
